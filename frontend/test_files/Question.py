4b4af711b839d7589ce1bcfe8d866f78d18be28c
jakkii <licc06ca@gmail.com>
2025-01-11 21:00:51 -0500

    Erased Test Files

--- frontend/test_files/Question.py
+++
@@ -1,8 +0,0 @@
-class Question:
-    def __init__(self, title, choices, correct_choice):
-        self.title = title
-        self.choices = choices
-        self.correct_choice = correct_choice
-
-    def is_correct(self, choice):
-        return choice == self.correct_choice
